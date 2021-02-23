43357a1f0f5b92314c76b1312063d64237570ec8
纪皓阳 <522423918@qq.com>
2021-02-23 16:26:38 +0800

    代码规范修改

--- src/main/java/com/webProject/blog/BlogApplication.java
+++ src/main/java/com/webProject/blog/BlogApplication.java
@@ -1,4 +1,4 @@
-package com.webProject.blog;
+package com.webproject.blog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
