e79f4c99a22d24be417c1d0d0336c4e15f3837ae
纪皓阳 <522423918@qq.com>
2021-04-30 17:30:23 +0800

    提交变更

--- src/main/java/com/webProject/blog/BlogApplication.java
+++ src/main/java/com/webProject/blog/BlogApplication.java
@@ -2,8 +2,13 @@ package com.webproject.blog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+/**
+ * @author jihaoyang
+ */
 @SpringBootApplication
+@EnableDiscoveryClient
 public class BlogApplication {
     public static void main(String[] args) {
         SpringApplication.run(BlogApplication.class, args);
